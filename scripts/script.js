9fff17ffe03fda68bdbe40c3511df611f54e904c
soofg <70442106+soofg@users.noreply.github.com>
2020-09-28 16:10:37 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
-// JavaScript Document
+var menuknop = document.querySelector('button');
+var menulijst = document.querySelector('ul');
+
+function toonMenu() {
+ menulijst.classList.toggle('toonMenu');
+}
+
+menuknop.addEventListener('click', toonMenu);
